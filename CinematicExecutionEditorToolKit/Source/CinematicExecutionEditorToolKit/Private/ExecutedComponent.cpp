336f79c3a59b0d2f2bf9d91f8c1c759d5ff8db3c
虚幻小子 <1480746031@qq.com>
2025-12-24 01:29:50 +0800

    提交

---
+++ CinematicExecutionEditorToolKit/Source/CinematicExecutionEditorToolKit/Private/ExecutedComponent.cpp
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ExecutedComponent.h"
+
+#include "Components/WidgetComponent.h"
+#include "GameFramework/Character.h"
+
+
+// Sets default values for this component's properties
+UExecutedComponent::UExecutedComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UExecutedComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	AddWidgetComponent();
+}
+
+
+// Called every frame
+void UExecutedComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                       FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+ACharacter* UExecutedComponent::GetOwnerCharacter()
+{
+	return Cast<ACharacter>(GetOwner());
+}
+
+void UExecutedComponent::AddWidgetComponent()
+{
+	GetOwner()->Modify();
+	WidgetComponent = NewObject<UWidgetComponent>(GetOwner());
+	WidgetComponent->OnComponentCreated();
+	FAttachmentTransformRules Rules(EAttachmentRule::SnapToTarget, EAttachmentRule::KeepWorld,
+	                                EAttachmentRule::KeepWorld, false);
+	WidgetComponent->AttachToComponent(GetOwnerCharacter()->GetMesh(), Rules, ScoketName);
+	WidgetComponent->RegisterComponent();
+
+	//设置控件参数
+	WidgetComponent->AddWorldOffset(WidgetLocation);
+	WidgetComponent->SetWidgetSpace(EWidgetSpace::Screen);
+	WidgetComponent->SetVisibility(false,false);
+	if (WidgetClass)
+	{
+		WidgetComponent->SetWidgetClass(WidgetClass);
+	}
+
+	WidgetComponent->SetDrawAtDesiredSize(true);
+	GetOwner()->AddInstanceComponent(WidgetComponent);
+}
+
+void UExecutedComponent::SetCanExecute(bool bCanExecute)
+{
+	if (bCanExecute)
+	{
+		WidgetComponent->SetVisibility(true,false);
+	}else
+	{
+		WidgetComponent->SetVisibility(false,false);
+	}
+}
---
+++ CinematicExecutionEditorToolKit/Source/CinematicExecutionEditorToolKit/Public/ExecutedComponent.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ExecutedComponent.generated.h"
+
+class UWidgetComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class CINEMATICEXECUTIONEDITORTOOLKIT_API UExecutedComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UExecutedComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	ACharacter* GetOwnerCharacter();
+	
+	/*
+	 *控件组件
+	 */
+	void AddWidgetComponent();
+	void SetCanExecute(bool bCanExecute);
+	UPROPERTY(VisibleInstanceOnly,meta = (AllowPrivateAccess = "true"))
+	UWidgetComponent* WidgetComponent;
+	
+	UPROPERTY(EditDefaultsOnly)
+	FName ScoketName = "Spine_03";
+
+	UPROPERTY(EditDefaultsOnly)
+	FVector WidgetLocation;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> WidgetClass;
+};
